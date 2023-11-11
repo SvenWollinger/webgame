a71d75826dd97d2c41df796cd154cde1f288bb8a
SvenWollinger <accounts@wollinger.email>
2023-11-11 20:08:14 +0100

    funky

--- src/main/kotlin/io/wollinger/webgame/Engine.kt
+++ src/main/kotlin/io/wollinger/webgame/Engine.kt
@@ -23,21 +23,37 @@ class Engine(
 
     val levelCollision = ArrayList<Rectangle>()
 
+    private val useLevelData = ArrayList<ArrayList<Char>>()
+
     init {
         window.requestAnimationFrame(::loop)
         objects.add(Player(Vector2(1.0, 1.0)))
         level.data.forEachIndexed { index1, line ->
+            val l = ArrayList<Char>()
+            useLevelData.add(l)
             line.forEachIndexed { index, c ->
-                if(c != '0') {
-                    levelCollision.add(Rectangle(index.toDouble(), index1.toDouble(), 1.0, 1.0))
-                }
+                l.add(c)
+            }
+        }
+        updateLevelCollision()
+    }
+
+    fun updateLevelCollision() {
+        levelCollision.clear()
+        useLevelData.forEachIndexed { y, parts ->
+            parts.forEachIndexed { x, part ->
+                if(part != '0')
+                levelCollision.add(Rectangle(x.toDouble(), y.toDouble(), 1.0, 1.0))
             }
         }
     }
 
     private fun update(delta: Double) {
         objects.forEach {
-            it.update(input, delta, levelCollision)
+            it.update(input, delta, levelCollision) {
+                useLevelData.get(it.y.toInt()).set(it.x.toInt(), '0')
+                updateLevelCollision()
+            }
         }
         val player = (objects.first() as Player)
         val playerPixelX = player.position.x * size()
@@ -68,8 +84,8 @@ class Engine(
         ctx.fillText("Grounded: ${player.grounded}", 0.0, size * 3)
 
         //Draw level
-        level.data.forEachIndexed { index, line ->
-            line.forEachIndexed { index2, char ->
+        useLevelData.forEachIndexed { index, parts ->
+            parts.forEachIndexed { index2, char ->
                 val draw = when(char) {
                     '1' -> block1
                     '2' -> block2
@@ -79,6 +95,7 @@ class Engine(
                 draw?.let { ctx.drawImage(draw, index2 * size + (screenX * size), index * size, size, size) }
             }
         }
+
         objects.forEach {
             it.draw(ctx, screenX, size)
         }
--- src/main/kotlin/io/wollinger/webgame/Entity.kt
+++ src/main/kotlin/io/wollinger/webgame/Entity.kt
@@ -5,7 +5,7 @@ import org.w3c.dom.CanvasRenderingContext2D
 import org.w3c.dom.Image
 
 open class Entity(val position: Vector2 = Vector2()) {
-    open fun update(input: Input, delta: Double, coll: ArrayList<Rectangle>) {
+    open fun update(input: Input, delta: Double, coll: ArrayList<Rectangle>, onHit: (Rectangle) -> Unit) {
 
     }
 
@@ -17,18 +17,18 @@ open class Entity(val position: Vector2 = Vector2()) {
 class Player(position: Vector2): Entity(position) {
     private val mario = Image().apply { src = "/img/mario.png" }
     private val speed = 8.0
-    private val gravity = 19.0
+    private val gravity = 30.0
     private val bbox = Rectangle(0.0, 0.0, 1.0, 1.0)
     private val effectiveBbox = Rectangle()
     val velocity = Vector2()
     var grounded = false
 
-    override fun update(input: Input, delta: Double, coll: ArrayList<Rectangle>) {
+    override fun update(input: Input, delta: Double, coll: ArrayList<Rectangle>, onHit: (Rectangle) -> Unit) {
         effectiveBbox.from(position.x, position.y, bbox.width, bbox.height)
 
         if(grounded) velocity.y = 0.0
 
-        if(input.isPressed(" ") && grounded) velocity.y -= 12.0
+        if(input.isPressed(" ") && grounded) velocity.y -= 22.0
         grounded = false
 
         if(input.isPressed("d")) velocity.x = speed
@@ -36,18 +36,34 @@ class Player(position: Vector2): Entity(position) {
         else velocity.x = 0.0
         velocity.y += gravity * delta
 
-        effectiveBbox.y += velocity.y * delta
-        val cY = coll.any { it.intersects(effectiveBbox) }
-        if(!cY) position.y = effectiveBbox.y
-        else {
-            grounded = coll.any { it.intersects(Rectangle(position.x, position.y + 0.1F, bbox.width, bbox.height)) }
-            if(coll.any { it.intersects(Rectangle(position.x, position.y - 0.1F, bbox.width, bbox.height)) }) velocity.y = 0.0
+        var hitHEad: Rectangle? = null
+        val s = (velocity.y * delta) / 20
+        repeat(20) {
+            effectiveBbox.y += s
+            val cY = coll.any { it.intersects(effectiveBbox) }
+            if(!cY) position.y = effectiveBbox.y
+            else {
+                grounded = coll.any { it.intersects(Rectangle(position.x, position.y + 0.1F, bbox.width, bbox.height)) }
+                val hit = coll.firstOrNull() { it.intersects(Rectangle(position.x, position.y - 0.1F, bbox.width, bbox.height)) }
+                if(hit != null) {
+                    hitHEad = hit
+                    velocity.y = 0.0
+                }
+            }
         }
 
-        effectiveBbox.x += velocity.x * delta
-        effectiveBbox.y = position.y//if(velocity.y > 0) 0.1F else -0.2F
-        val cX = coll.any { it.intersects(effectiveBbox) }
-        if(!cX) position.x = effectiveBbox.x
+        val s2 = (velocity.x * delta) / 20
+        repeat(20) {
+            effectiveBbox.x += s2
+            effectiveBbox.y = position.y//if(velocity.y > 0) 0.1F else -0.2F
+            val cX = coll.any { it.intersects(effectiveBbox) }
+            if(!cX) position.x = effectiveBbox.x
+        }
+
+        if(hitHEad != null) {
+            println("Hit: $hitHEad")
+            onHit.invoke(hitHEad!!)
+        }
     }
 
     override fun draw(ctx: CanvasRenderingContext2D, screenX: Double, size: Double) {
