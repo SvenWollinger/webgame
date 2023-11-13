41e9b837a46819d23b721b19ef136a2429b37896
SvenWollinger <accounts@wollinger.email>
2023-11-13 13:50:59 +0100

    add build.json test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
+import kotlinx.serialization.SerializationStrategy
+import kotlinx.serialization.descriptors.*
+import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.encoding.encodeStructure
+import kotlinx.serialization.json.Json
+
 plugins {
     kotlin("js") version "1.8.20"
     kotlin("plugin.serialization") version "1.8.20"
@@ -10,6 +16,15 @@ repositories {
     mavenCentral()
 }
 
+buildscript {
+    dependencies {
+
+        classpath("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+
+    }
+}
+
+
 dependencies {
     testImplementation(kotlin("test"))
     implementation(kotlin("stdlib-js"))
@@ -17,14 +32,37 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 }
 
+data class BuildInfo(
+    val githash: String,
+    val timestamp: Long
+)
 
 kotlin {
     js(IR) {
         browser {
             webpackTask {
+                File("src/main/resources/build.json").apply {
+                    createNewFile()
+                    val bi = BuildInfo("", System.currentTimeMillis())
+                    writeText(Json.encodeToString(serializer = object: SerializationStrategy<BuildInfo> {
+                        override val descriptor = buildClassSerialDescriptor("BuildInfo") {
+                            element<String>("githash")
+                            element<Long>("timestamp")
+                        }
+
+                        override fun serialize(encoder: Encoder, value: BuildInfo) {
+                            encoder.encodeStructure(descriptor) {
+                                encodeStringElement(descriptor, 0, value.githash)
+                                encodeLongElement(descriptor, 1, value.timestamp)
+                            }
+                        }
+
+                    }, bi))
+                }
                 this.outputFileName = "app.js"
             }
         }
         binaries.executable()
     }
 }
+
